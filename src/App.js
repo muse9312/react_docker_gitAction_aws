0ffb081cec28f38464dd63b163e6553fa141ceb2
muse9312 <muse9312@naver.com>
2022-04-12 09:37:13 +0900

    modify

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          졸려뒤져
+          졸려
         </p>
         <a
           className="App-link"
